88f541c6349adf6cc36a1c8a71164e7acd7986d1
Kiel Dehnert <kielzor@gmail.com>
2019-07-29 22:14:42 -0600

    Reformat and rework layout 7-29-19

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { AboutComponent } from './about/about.component';
+import { ServicesComponent } from './services/services.component';
+import { TestimonialsComponent } from './testimonials/testimonials.component';
+import { ContactFormComponent } from './contact-form/contact-form.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    AboutComponent
+    AboutComponent,
+    ServicesComponent,
+    TestimonialsComponent,
+    ContactFormComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-  public subTitle = 'Something Something give me all your money';
+  public nav = 'about';
+  // public subTitle = 'Something Something give me all your money';
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  public handleClick(nav) {
+    this.nav = nav;
+    console.log('yay', nav)
   }
 }
